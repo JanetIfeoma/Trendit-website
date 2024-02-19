71a77c16025e2bb30074301867abbc5b05064d1b
JanetIfeoma <108590608+JanetIfeoma@users.noreply.github.com>
2024-02-19 17:24:05 +0100

    1st commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// hamburger menu
+
+document.addEventListener("DOMContentLoaded", function() {
+    var hamburgerMenu = document.querySelector('.hamburger');
+    var navBar = document.querySelector('.nav-bar');
+
+    hamburgerMenu.addEventListener('click', function() {
+        navBar.classList.toggle('active');
+    });
+});
